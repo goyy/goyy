fd04015dd8fc2a962faf4b473c7142d42b69a5a9
goyy <goyy@21cn.com>
2015-10-01 20:50:11 +0800

    run generate.bat

--- data/xsql/users.sql
+++ data/xsql/users.sql
@@ -1,50 +1,50 @@
 
--- MYSQL
-DROP TABLE IF EXISTS USERS;
-CREATE TABLE USERS (
-  ID VARCHAR(50) NOT NULL,
-  CODE VARCHAR(50),
-  NAME VARCHAR(255),
-  PASSWORD VARCHAR(255),
-  MEMO VARCHAR(1000),
-  GENRE VARCHAR(50),
-  STATUS VARCHAR(50),
-  ROLES VARCHAR(1000),
-  POSTS VARCHAR(1000),
-  ORG VARCHAR(50),
-  AREA VARCHAR(50),
-  CREATER VARCHAR(50),
-  CREATED BIGINT,
-  MODIFIER VARCHAR(50),
-  MODIFIED BIGINT,
-  VERSION INT(11),
-  DELETION INT(11),
-  PRIMARY KEY (ID)
+-- mysql
+drop table if exists users;
+create table users (
+  id varchar(50) not null,
+  code varchar(50),
+  name varchar(255),
+  password varchar(255),
+  memo varchar(1000),
+  genre varchar(50),
+  status varchar(50),
+  roles varchar(1000),
+  posts varchar(1000),
+  org varchar(50),
+  area varchar(50),
+  creater varchar(50),
+  created bigint,
+  modifier varchar(50),
+  modified bigint,
+  version int(11),
+  deletion int(11),
+  primary key (id)
 );
 
--- ORACLE
-CREATE TABLE USERS (
-  ID VARCHAR2(50) NOT NULL,
-  CODE VARCHAR2(50),
-  NAME VARCHAR2(255),
-  PASSWORD VARCHAR2(255),
-  MEMO VARCHAR2(1000),
-  GENRE VARCHAR2(50),
-  STATUS VARCHAR2(50),
-  ROLES VARCHAR2(1000),
-  POSTS VARCHAR2(1000),
-  ORG VARCHAR2(50),
-  AREA VARCHAR2(50),
-  CREATER VARCHAR2(50),
-  CREATED NUMBER(20),
-  MODIFIER VARCHAR2(50),
-  MODIFIED NUMBER(20),
-  VERSION NUMBER(11),
-  DELETION NUMBER(11),
-  PRIMARY KEY (ID)
+-- oracle
+create table users (
+  id varchar2(50) not null,
+  code varchar2(50),
+  name varchar2(255),
+  password varchar2(255),
+  memo varchar2(1000),
+  genre varchar2(50),
+  status varchar2(50),
+  roles varchar2(1000),
+  posts varchar2(1000),
+  org varchar2(50),
+  area varchar2(50),
+  creater varchar2(50),
+  created number(20),
+  modifier varchar2(50),
+  modified number(20),
+  version number(11),
+  deletion number(11),
+  primary key (id)
 );
 
-INSERT INTO USERS (ID, CODE, NAME, PASSWORD, MEMO, GENRE, STATUS, ROLES, POSTS, ORG, AREA, CREATER, CREATED, MODIFIER, MODIFIED, VERSION, DELETION) VALUES ('demo-i-01', '01', '01', '01', '01', '01', '01', '01', '01', '01', '01', '01', 1443253329, '01', 1443253329, 0, 0);
-INSERT INTO USERS (ID, CODE, NAME, PASSWORD, MEMO, GENRE, STATUS, ROLES, POSTS, ORG, AREA, CREATER, CREATED, MODIFIER, MODIFIED, VERSION, DELETION) VALUES ('demo-i-02', '02', 'user02', '02', '02', '02', '02', '02', '02', '02', '02', '02', 1443253329, '02', 1443253329, 0, 1);
-INSERT INTO USERS (ID, CODE, NAME, PASSWORD, MEMO, GENRE, STATUS, ROLES, POSTS, ORG, AREA, CREATER, CREATED, MODIFIER, MODIFIED, VERSION, DELETION) VALUES ('demo-i-03', '03', '03', '03', '03', '03', '03', '03', '03', '03', '03', '03', 1443253329, '03', 1443253329, 0, 0);
-INSERT INTO USERS (ID, CODE, NAME, PASSWORD, MEMO, GENRE, STATUS, ROLES, POSTS, ORG, AREA, CREATER, CREATED, MODIFIER, MODIFIED, VERSION, DELETION) VALUES ('demo-i-04', '04', '04', '04', '04', '04', '04', '04', '04', '04', '04', '04', 1443253329, '04', 1443253329, 0, 0);
+insert into users (id, code, name, password, memo, genre, status, roles, posts, org, area, creater, created, modifier, modified, version, deletion) values ('demo-i-01', '01', '01', '01', '01', '01', '01', '01', '01', '01', '01', '01', 1443253329, '01', 1443253329, 0, 0);
+insert into users (id, code, name, password, memo, genre, status, roles, posts, org, area, creater, created, modifier, modified, version, deletion) values ('demo-i-02', '02', 'user02', '02', '02', '02', '02', '02', '02', '02', '02', '02', 1443253329, '02', 1443253329, 0, 1);
+insert into users (id, code, name, password, memo, genre, status, roles, posts, org, area, creater, created, modifier, modified, version, deletion) values ('demo-i-03', '03', '03', '03', '03', '03', '03', '03', '03', '03', '03', '03', 1443253329, '03', 1443253329, 0, 0);
+insert into users (id, code, name, password, memo, genre, status, roles, posts, org, area, creater, created, modifier, modified, version, deletion) values ('demo-i-04', '04', '04', '04', '04', '04', '04', '04', '04', '04', '04', '04', 1443253329, '04', 1443253329, 0, 0);
