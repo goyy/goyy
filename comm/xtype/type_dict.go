72a377622add451acaf4eb5698d0c4d28e2b5a5a
goyy <goyy@21cn.com>
2016-08-17 16:16:28 +0800

    Add xtype.Dict

---
+++ comm/xtype/type_dict.go
@@ -0,0 +1,15 @@
+// Copyright 2014 The goyy Authors.  All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package xtype
+
+type Dict struct {
+	Mkey    string `json:"mkey"`
+	Mval    string `json:"mval"`
+	Genre   string `json:"genre"`
+	Filters string `json:"filters"`
+	Descr   string `json:"descr"`
+	Ordinal string `json:"ordinal"`
+	Created string `json:"created"`
+}
