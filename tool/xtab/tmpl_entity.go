f48b4cd2e940ba4cc731baf0e222945f47e10e58
goyy <goyy@21cn.com>
2016-09-20 19:09:36 +0800

    Add support to generate menu code

--- tool/xtab/tmpl_entity.go
+++ tool/xtab/tmpl_entity.go
@@ -11,7 +11,7 @@ import (
 	"gopkg.in/goyy/goyy.v0/data/schema"
 )
 
-//go:generate xgen -entity=$GOFILE -scaffold{{if notblank .Module.Clidir}} -clidir={{.Module.Clidir}}{{end}}{{if notblank .Module.Clipath}} -clipath={{.Module.Clipath}}{{end}} -apipath={{.Module.Apipath}} -tstpath={{.Module.Tstpath}}
+//go:generate xgen -entity=$GOFILE -scaffold{{if notblank .Module.Clipath}} -clipath={{.Module.Clipath}}{{end}} -apipath={{.Module.Apipath}} -tstpath={{.Module.Tstpath}}
 
 // {{.Name}}` + i18N.Message("domain.title") + `.
 // @entity(module:"{{.Id}}" project:"{{.Module.Id}}"{{if notblank .Slave}} relationship:"slave"{{end}})
