c8d3c23351b735330df0d4c702a1e461a434409d
goyy <goyy@21cn.com>
2016-10-12 11:16:12 +0800

    Perfect sys module

--- app/sys/internal/dict/dict_sql.go
+++ app/sys/internal/dict/dict_sql.go
@@ -2,6 +2,4 @@ package dict
 
 const (
 	ListByGenre = `select distinct genre from sys_dict where deletion = 0 order by genre`
-
-	CountGenre = `select count(id) from sys_dict where genre = ? and mkey = ?`
 )
