460999b457eeec5a82783c5de3f6a06d82bc0ef5
goyy <goyy@21cn.com>
2017-01-16 17:12:50 +0800

    rename : bms -> adm

--- tool/xtab/tmpl_menu.go
+++ tool/xtab/tmpl_menu.go
@@ -26,20 +26,20 @@ var tmplMenu = `<script type="text/javascript">{{$ := .}}
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="/sys/user/info.html">{{$.Project.Name}}</a>
+				<a class="navbar-brand" go:href="/home.html?{%ver%}">{{$.Project.Name}}</a>
 			</div>
 			<div class="navbar-collapse collapse">{{range $module := .Modules}}
 				<ul class="nav navbar-nav">
-					<li id="header_{{$module.Id}}" data-permissions="{{range $table := $.Tables}}{{if eq $table.Module.Id $module.Id}}{{if eq $table.Menu "true"}}{{if ne $table.Id "-"}}{{$table.Permissions}},{{end}}{{end}}{{end}}{{end}}" class="{%if eq .param '{{$module.Id}}'%}active {%end%}dropdown">
+					<li id="header_{{$module.Id}}" go:data-permissions="{{range $table := $.Tables}}{{if eq $table.Module.Id $module.Id}}{{if eq $table.Menu "true"}}{{if ne $table.Id "-"}}{{$table.Permissions}}{{end}}{{end}}{{end}}{{end}}" class="{%if eq .param '{{$module.Id}}'%}active {%end%}dropdown">
 						<a href="#" class="dropdown-toggle" data-toggle="dropdown">{{$module.Name}}<b class="caret"></b></a>
 						<ul class="dropdown-menu">{{range $table := $.Tables}}{{if eq $table.Module.Id $module.Id}}{{if eq $table.Menu "true"}}{{if eq $table.Id "-"}}
 							<li data-permissions="{{$table.Permissions}}" class="divider"></li>{{else}}
-							<li data-permissions="{{$table.Permissions}}"><a href="{{$table.Href}}">{{$table.Name}}{{message "tmpl.menu.manage"}}</a></li>{{end}}{{end}}{{end}}{{end}}
+							<li data-permissions="{{$table.Permissions}}"><a go:href="{{$table.Href}}?{%ver%}">{{$table.Name}}{{message "tmpl.menu.manage"}}</a></li>{{end}}{{end}}{{end}}{{end}}
 						</ul>
 					</li>
 				</ul>{{end}}
 				<ul class="nav navbar-nav navbar-right">
-					<li><a id="loginName" href="/sys/user/info.html">&nbsp;</a></li>
+					<li><a id="loginName" go:href="/home.html?{%ver%}">&nbsp;</a></li>
 					<li><a href="/logout">{{message "tmpl.menu.exit"}}</a></li>
 				</ul>
 			</div>
