51c5844df4b60b4f52e5cc761fb3eedd6036edf3
goyy <goyy@21cn.com>
2017-02-07 18:47:35 +0800

    Add support to new project on xgen

--- app/sys/internal/area/area_entity.go
+++ app/sys/internal/area/area_entity.go
@@ -5,7 +5,7 @@ import (
 	"gopkg.in/goyy/goyy.v0/data/schema"
 )
 
-//go:generate xgen -entity=$GOFILE -scaffold -clipath=gopkg.in/goyy/goyy.v0/app/adm -apipath=gopkg.in/goyy/goyy.v0/app/sys -tstpath=gopkg.in/goyy/goyy.v0/app/tst
+//go:generate xgen -entity=$GOFILE -scaffold -admpath=gopkg.in/goyy/goyy.v0/app/adm -apipath=gopkg.in/goyy/goyy.v0/app/sys -tstpath=gopkg.in/goyy/goyy.v0/app/tst
 
 // AREA Entity.
 // @entity(module:"area" project:"sys")
