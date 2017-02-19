73eead127a02f4073b35c21c2454caf15ef43824
goyy <goyy@21cn.com>
2017-02-19 21:58:04 +0800

    Add support to new project on xgen

--- tool/xtab/tmpl_data_user.go
+++ tool/xtab/tmpl_data_user.go
@@ -5,6 +5,6 @@
 package main
 
 var tmplDataUser = `
-insert into {{case "SYS_USER (ID, NAME, CODE, PASSWD, GENRE, EMAIL, TEL, MOBILE, AREA_ID, ORG_ID, LOGIN_NAME, LOGIN_IP, LOGIN_TIME, MEMO, CREATES, CREATER, CREATED, MODIFIER, MODIFIED, VERSION, DELETION, ARTIFICAL, HISTORY)"}}
-values ('admin', '{{message "tmpl.data.user.admin"}}', null, '92d55a4a6b07', '10', 'admin@goyy.org', null, null, 'root', 'root', 'admin', null, ` + now + `, null, null, null, ` + now + `, null, ` + now + `, 0, 0, 0, 0){{seperator}}
+insert into {{case "SYS_USER (ID, NAME, CODE, PASSWD, GENRE, EMAIL, TEL, MOBILE, AREA_ID, ORG_ID, DIMISSION_TIME, FREEZE_TIME, LOGIN_NAME, LOGIN_IP, LOGIN_TIME, MEMO, CREATES, CREATER, CREATED, MODIFIER, MODIFIED, VERSION, DELETION, ARTIFICAL, HISTORY)"}}
+values ('admin', '{{message "tmpl.data.user.admin"}}', null, '92d55a4a6b07', '10', 'admin@goyy.org', null, null, 'root', 'root', ` + now + `, ` + now + `, 'admin', null, ` + now + `, null, null, null, ` + now + `, null, ` + now + `, 0, 0, 0, 0){{seperator}}
 `
